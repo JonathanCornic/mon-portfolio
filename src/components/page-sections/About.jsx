da9135599cff2acea6ec448268b5ba192fbc0676
Jonathan Cornic <117487357+JonathanCornic@users.noreply.github.com>
2023-08-04 10:54:32 +0200

    about section created

--- src/components/page-sections/About.jsx
+++ src/components/page-sections/About.jsx
@@ -1,9 +1,38 @@
-
-
 export default function About() {
-  return (
-    <section id="A propos" className="section">
-      A propos
-    </section>
-  )
+    return (
+        <section id="A propos" className="section">
+            <div className="max-w-screen-lg p-10 flex flex-col justify-center text-center lg:text-left ">
+                <div className="py-5">
+                    <h2 className="mb-10 lg:mb-20 text-gradient">
+                        A propos de moi
+                    </h2>
+                </div>
+                <p className="">
+                    Enchanté, je suis Jonathan Cornic, un développeur web
+                    passionné avec une expertise particulière en React. Mon
+                    voyage dans le monde du développement a débuté il y a
+                    plusieurs mois, et depuis, je suis tombé amoureux de la
+                    création d'expériences interactives et intuitives sur le
+                    Web.
+                </p>
+                <br />
+                <p className="">
+                    Au fil du temps, j'ai eu l'occasion de travailler sur une
+                    multitude de projets, allant des petites applications aux
+                    systèmes web complexes. Cette diversité m'a permis d'affiner
+                    mes compétences et de m'adapter à différentes situations,
+                    tout en conservant un engagement constant envers la qualité
+                    et l'efficacité du code.
+                </p>
+                <br />
+                <p className="">
+                    En tant que développeur, je suis toujours avide d'apprendre
+                    et de rester à l'affût des dernières tendances et des
+                    meilleures pratiques du secteur. Je suis convaincu que
+                    l'apprentissage continu est la clé pour rester pertinent et
+                    offrir des solutions innovantes à mes clients.
+                </p>
+            </div>
+        </section>
+    )
 }
--- src/components/page-sections/Navbar.jsx
+++ src/components/page-sections/Navbar.jsx
@@ -36,7 +36,7 @@ export default function Navbar() {
     const [activeSection, setActiveSection] = useState('Accueil')
 
     return (
-        <nav className="fixed z-50 w-full px-2 lg:px-0 lg:mix-blend-exclusion lg:backdrop-blur-sm">
+        <nav className="fixed z-50 w-full px-2 lg:px-0 lg:mix-blend-exclusion lg:backdrop-blur-sm max-w-[2560px]">
             <div className="min-w-[250px] max-w-[420px] mx-auto rounded-full bg-black/50 backdrop-blur-sm px-3 py-2 mt-4 flex items-center justify-center lg:justify-between lg:bg-inherit lg:rounded-none lg:mt-0 lg:max-w-full">
                 <div className="cursor-pointer bg-primary hidden hover:bg-gradient lg:block ">
                     <Link to="Accueil" smooth={true} duration={500}>
