97e8868bcd3afd2597d370e19e273113c3ef6334
Jonathan Cornic <117487357+JonathanCornic@users.noreply.github.com>
2023-08-09 20:13:32 +0200

    animation section form

--- src/components/page-sections/Contact.jsx
+++ src/components/page-sections/Contact.jsx
@@ -1,48 +1,89 @@
 import useContactForm from '../../functions/useContactForm'
+import { motion } from 'framer-motion'
+import fadeIn from '../../variants'
 
 export default function Contact() {
     const { formData, handleChange, handleSubmit } = useContactForm()
 
     return (
-        <form onSubmit={handleSubmit} className="section" id="Contact">
-            <div>
-                <label htmlFor="name">Nom:</label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder="Votre nom"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                />
+        <section id="Contact" className="section">
+            <div className="container mx-auto">
+                <div className="flex flex-col lg:flex-row gap-x-10 py-10">
+                    <motion.div
+                        variants={fadeIn('right', 0.3)}
+                        initial="hidden"
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.3 }}
+                        className="flex-1 flex flex-col items-start justify-center py-5"
+                    >
+                        <h2 className="text-gradient">Un projet ?</h2>
+                        <p className="text-3xl lg:text-6xl xl:text-8xl">
+                            Travaillons <br /> ensemble!
+                        </p>
+                    </motion.div>
+                    <motion.form
+                        onSubmit={handleSubmit}
+                        variants={fadeIn('left', 0.3)}
+                        initial="hidden"
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.3 }}
+                        className="flex-1 border rounded-2xl flex flex-col gap-y-6 p-5 pb-10"
+                    >
+                        <div className="w-full">
+                            <label htmlFor="name" className="invisible">
+                                Nom:
+                            </label>
+                            <input
+                                className="bg-transparent border-b py-3 outline-none w-full focus:border-yellow-700 transition-all duration-1000"
+                                type="text"
+                                id="name"
+                                name="name"
+                                placeholder="Votre nom"
+                                value={formData.name}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className="w-full">
+                            <label htmlFor="email" className="invisible">
+                                Email:
+                            </label>
+                            <input
+                                className="bg-transparent border-b py-3 outline-none w-full focus:border-yellow-700 transition-all duration-1000 "
+                                type="email"
+                                id="email"
+                                name="email"
+                                placeholder="Votre email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className="w-full">
+                            <label htmlFor="message" className="invisible">
+                                Message:
+                            </label>
+                            <textarea
+                                className="bg-transparent border-b py-3 outline-none w-full focus:border-yellow-700 transition-all duration-1000"
+                                id="message"
+                                name="message"
+                                placeholder="Votre message"
+                                value={formData.message}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <button
+                                type="submit"
+                                className="btn hover:scale-110 duration-700"
+                            >
+                                Envoyer
+                            </button>
+                        </div>
+                    </motion.form>
+                </div>
             </div>
-            <div>
-                <label htmlFor="email">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Entrez votre email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="message">Message:</label>
-                <textarea
-                    id="message"
-                    name="message"
-                    placeholder="Entrez votre message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div>
-                <button type="submit">Envoyer</button>
-            </div>
-        </form>
+        </section>
     )
 }
