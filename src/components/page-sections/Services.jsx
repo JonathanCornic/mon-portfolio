a9dc2b3dbf3533b8170de85282b93866ade24cfb
Jonathan Cornic <117487357+JonathanCornic@users.noreply.github.com>
2023-08-10 07:46:12 +0200

    section services created

--- src/components/page-sections/Services.jsx
+++ src/components/page-sections/Services.jsx
@@ -1,8 +1,58 @@
-
+import services from '../../data/serviceData'
+import { motion } from 'framer-motion'
+import fadeIn from '../../variants'
 export default function Services() {
-  return (
-    <div id="Services" className="section text-primary">
-      Services
-    </div>
-  )
+    return (
+        <div id="Services" className="section">
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row xl:items-center">
+                    <motion.div
+                        variants={fadeIn('down', 0.3)}
+                        initial="hidden"
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.3 }}
+                        className="flex-1"
+                    >
+                        <h2>Mes services</h2>
+                        <p>
+                            Expérience et Expertise au Service de Votre Vision
+                        </p>
+                        <h3 className="py-5">Ma stack téchnologique</h3>
+                        <ul className="flex gap-x-1 flex-wrap">
+                            <li>MongoDB</li>
+                            <li>ExpressJS</li>
+                            <li>React</li>
+                            <li>NodeJS</li>
+                            <li>tailwindCSS</li>
+                            <li>firebase</li>
+                        </ul>
+                        <h3 className="py-5">Prochainement</h3>
+                        <ul>
+                            <li>Typescript</li>
+                            <li>NextJS</li>
+                        </ul>
+                    </motion.div>
+                    <div className="flex-1">
+                        <ul>
+                            {services.map(({ id, name, description }) => {
+                                return (
+                                    <motion.li
+                                        key={id}
+                                        variants={fadeIn('up', 0.3)}
+                                        initial="hidden"
+                                        whileInView={'show'}
+                                        viewport={{ once: false, amount: 0.3 }}
+                                        className="border-b"
+                                    >
+                                        <h3 className="py-3">{name}</h3>
+                                        <p className="pb-3">{description}</p>
+                                    </motion.li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
---
+++ src/data/serviceData.jsx
@@ -0,0 +1,34 @@
+const services = [
+    {
+        id: 1,
+        name: 'Développement',
+        description:
+            "Création d'applications web modernes et réactives en utilisant la bibliothèque React. Conception et développement personnalisés pour répondre aux besoins spécifiques de chaque projet.",
+    },
+    {
+        id: 2,
+        name: 'Conception',
+        description:
+            "Création d'interfaces utilisateur attrayantes, intuitives et conviviales en utilisant les meilleures pratiques de conception. Mise en œuvre de maquettes et de designs dans un format réactif.",
+    },
+    {
+        id: 3,
+        name: 'Intégration',
+        description:
+            "Développement de composants React réutilisables et modulaires pour améliorer l'efficacité du développement et maintenir la cohérence tout au long de l'application.",
+    },
+    {
+        id: 4,
+        name: 'Optimisation',
+        description:
+            "Optimisation des performances des applications React pour garantir un chargement rapide, une réactivité élevée et une expérience utilisateur fluide, en utilisant des techniques telles que le code splitting et la gestion de l'état efficace.",
+    },
+    {
+        id: 5,
+        name: 'Hébergement',
+        description:
+            "Adepte de l'integration continue, je réalise l hébergement de vos applications sur les plateformes les plus populaires, tels que Heroku, Netlify et Firebase pendant le processus de création.",
+    },
+]
+
+export default services
