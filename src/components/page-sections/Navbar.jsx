f3bdcf35a456161abbe94ddc6e2d6bb68f551599
Jonathan Cornic <117487357+JonathanCornic@users.noreply.github.com>
2023-08-17 17:53:05 +0200

    update  new domaine url

--- src/components/page-sections/Navbar.jsx
+++ src/components/page-sections/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
     return (
         <nav className="fixed z-50 w-full px-2 lg:px-0 lg:mix-blend-exclusion lg:backdrop-blur-sm max-w-[2560px]">
             <div className="min-w-[250px] max-w-[420px] mx-auto rounded-full bg-black/50 backdrop-blur-sm px-3 py-2 mt-4 flex items-center justify-center lg:justify-between lg:bg-inherit lg:rounded-none lg:mt-0 lg:max-w-full">
-                <div className="cursor-pointer bg-primary hidden hover:bg-gradient lg:block ">
+                <div className="cursor-pointer bg-primary hidden lg:hover:bg-gradient lg:block ">
                     <Link to="Accueil" smooth={true} duration={500}>
                         <img src={Logo} alt="mon logo" className='w-14 h-14' />
                     </Link>
@@ -19,7 +19,7 @@ export default function Navbar() {
                     {links.map(({ id, link, icon }) => (
                         <li
                             key={id}
-                            className={`relative z-50 text-primary text-xl px-3 py-3 mx-1 rounded-full cursor-pointer hover:scale-110 duration-300 hover:text-gradient md:mx-4 ${
+                            className={`relative z-50 text-primary text-xl px-3 py-3 mx-1 rounded-full cursor-pointer lg:hover:scale-110 duration-300 lg:hover:text-gradient md:mx-4 ${
                                 link === activeSection ? 'active' : ''
                             }`}
                         >
